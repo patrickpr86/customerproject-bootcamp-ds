9b0b7481b2864c1a9e06eed88c8942f69866e407
Lucas Galvao <33298759+lucasengcomp@users.noreply.github.com>
2020-10-01 15:03:51 -0300

    Update Client.java
    Sugestão de pull request

--- backend/src/main/java/com/patrick/customerproject/entites/Client.java
+++ backend/src/main/java/com/patrick/customerproject/entites/Client.java
@@ -14,123 +14,112 @@ import javax.persistence.Table;
 
 @Entity
 @Table(name = "tb_client")
-public class Client implements Serializable{
-	private static final long serialVersionUID = 1L;
-	
-	@Id
-	@GeneratedValue(strategy = GenerationType.IDENTITY)
-	private Long id;
-	private String name;
-	private String cpf;
-	private Double income;
-	
-	
-	  @Column(columnDefinition = "TIME WITHOUT TIME ZONE")
-	  private Instant birthDate;
-	 
-	
-	private Integer children;
-	
-	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-	private Instant createdAt;
-	
-	@Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
-	private Instant updatedAt;
-	
-	public Client() {
-		
-	}
+public class Client implements Serializable {
+    private static final long serialVersionUID = 1L;
 
-	public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
-		super();
-		this.id = id;
-		this.name = name;
-		this.cpf = cpf;
-		this.income = income;
-		this.birthDate = birthDate; 
-		this.children = children;
-	}
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String cpf;
+    private Double income;
 
-	public Long getId() {
-		return id;
-	}
+    @Column(columnDefinition = "TIME WITHOUT TIME ZONE")
+    private Instant birthDate;
 
-	public void setId(Long id) {
-		this.id = id;
-	}
+    private Integer children;
 
-	public String getName() {
-		return name;
-	}
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant createdAt;
 
-	public void setName(String name) {
-		this.name = name;
-	}
+    @Column(columnDefinition = "TIMESTAMP WITHOUT TIME ZONE")
+    private Instant updatedAt;
 
-	public String getCpf() {
-		return cpf;
-	}
+    public Client() {
 
-	public void setCpf(String cpf) {
-		this.cpf = cpf;
-	}
+    }
 
-	public Double getIncome() {
-		return income;
-	}
+    public Client(Long id, String name, String cpf, Double income, Instant birthDate, Integer children) {
+        super();
+        this.id = id;
+        this.name = name;
+        this.cpf = cpf;
+        this.income = income;
+        this.birthDate = birthDate;
+        this.children = children;
+    }
 
-	public void setIncome(Double income) {
-		this.income = income;
-	}
+    public Long getId() {
+        return id;
+    }
 
-	
-	  public Instant getBirthDate() { return birthDate; }
-	  
-	  public void setBirthDate(Instant birthDate) { this.birthDate = birthDate; }
-	 
+    public void setId(Long id) {
+        this.id = id;
+    }
 
-	public Integer getChildren() {
-		return children;
-	}
+    public String getName() {
+        return name;
+    }
 
-	public void setChildren(Integer children) {
-		this.children = children;
-	}
+    public void setName(String name) {
+        this.name = name;
+    }
 
-	@Override
-	public int hashCode() {
-		final int prime = 31;
-		int result = 1;
-		result = prime * result + ((id == null) ? 0 : id.hashCode());
-		return result;
-	}
+    public String getCpf() {
+        return cpf;
+    }
 
-	@Override
-	public boolean equals(Object obj) {
-		if (this == obj)
-			return true;
-		if (obj == null)
-			return false;
-		if (getClass() != obj.getClass())
-			return false;
-		Client other = (Client) obj;
-		if (id == null) {
-			if (other.id != null)
-				return false;
-		} else if (!id.equals(other.id))
-			return false;
-		return true;
-	}
-	
-	@PrePersist
-	public void prePersist() {
-		createdAt = Instant.now();
-	}
-	
-	@PreUpdate
-	public void preUpdate() {
-		updatedAt = Instant.now();
-	}
-			
+    public void setCpf(String cpf) {
+        this.cpf = cpf;
+    }
 
+    public Double getIncome() {
+        return income;
+    }
+
+    public void setIncome(Double income) {
+        this.income = income;
+    }
+
+
+    public Instant getBirthDate() {
+        return birthDate = Instant.EPOCH;
+    }
+
+    public void setBirthDate(Instant birthDate) {
+        this.birthDate = birthDate;
+    }
+
+    public Integer getChildren() {
+        return children;
+    }
+
+    public void setChildren(Integer children) {
+        this.children = children;
+    }
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Client other = (Client) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
 }
