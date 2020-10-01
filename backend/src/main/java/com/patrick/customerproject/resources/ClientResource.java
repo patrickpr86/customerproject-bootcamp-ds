83a17c3ed3aa011d9245cc76227842b872e0a8ab
patrickpr86 <sh4dbr@gmail.com>
2020-10-01 19:02:33 -0300

    fix sql import values again

--- backend/src/main/java/com/patrick/customerproject/resources/ClientResource.java
+++ backend/src/main/java/com/patrick/customerproject/resources/ClientResource.java
@@ -1,10 +1,13 @@
 package com.patrick.customerproject.resources;
 
+import java.net.URI;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.data.domain.Sort.Direction;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -13,6 +16,7 @@ import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
 import com.patrick.customerproject.entites.dto.ClientDTO;
 import com.patrick.customerproject.services.ClientService;
@@ -48,13 +52,21 @@ public class ClientResource {
 	@PostMapping
 	public ResponseEntity<ClientDTO> insert(@RequestBody ClientDTO dto){
 		dto = clientService.insert(dto);
-		return ResponseEntity.ok().body(dto);
+		URI uri = ServletUriComponentsBuilder.fromCurrentRequest().path("/{id}").buildAndExpand(dto.getId()).toUri();
+		return ResponseEntity.created(uri).body(dto);
 	}
 	
 	@PutMapping(value = "/{id}")
 	public ResponseEntity<ClientDTO> update(@PathVariable Long id, @RequestBody ClientDTO dto) {
 		dto = clientService.update(id, dto);
 		return ResponseEntity.ok().body(dto);
+	}
+	
+	@DeleteMapping(value = "/{id}")
+	public ResponseEntity<ClientDTO> delete(@PathVariable Long id) {
+		clientService.delete(id);
+		return ResponseEntity.noContent().build();
+
 	}
 	
 }
--- backend/src/main/java/com/patrick/customerproject/services/ClientService.java
+++ backend/src/main/java/com/patrick/customerproject/services/ClientService.java
@@ -5,6 +5,8 @@ import java.util.Optional;
 import javax.persistence.EntityNotFoundException;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.dao.DataIntegrityViolationException;
+import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
@@ -13,6 +15,7 @@ import org.springframework.transaction.annotation.Transactional;
 import com.patrick.customerproject.entites.Client;
 import com.patrick.customerproject.entites.dto.ClientDTO;
 import com.patrick.customerproject.repositories.ClientRepository;
+import com.patrick.customerproject.services.exceptions.DatabaseException;
 import com.patrick.customerproject.services.exceptions.ResourceNotFoundException;
 
 @Service
@@ -33,15 +36,11 @@ public class ClientService {
 		Client entity = obj.get();
 		return new ClientDTO(entity);
 	}
-	
+
 	@Transactional
 	public ClientDTO insert(ClientDTO dto) {
 		Client entity = new Client();
-		entity.setName(dto.getName());
-		entity.setCpf(dto.getCpf());
-		entity.setIncome(dto.getIncome());
-		entity.setBirthDate(dto.getBirthDate());
-		entity.setChildren(dto.getChildren());
+		copyDtoToEntity(dto, entity);
 		entity = clientRepository.save(entity);
 		return new ClientDTO(entity);
 
@@ -50,19 +49,38 @@ public class ClientService {
 	@Transactional
 	public ClientDTO update(Long id, ClientDTO dto) {
 		try {
-		Client entity = clientRepository.getOne(id);
+			Client entity = clientRepository.getOne(id);
+			copyDtoToEntity(dto, entity);
+
+			entity = clientRepository.save(entity);
+			return new ClientDTO(entity);
+
+		} catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found!");
+		}
+
+	}
+	
+	public void delete(Long id) {
+		try {
+			clientRepository.deleteById(id);
+		} 
+		catch (EmptyResultDataAccessException e) {
+			throw new ResourceNotFoundException("Id not found " + id);
+		} 
+		catch (DataIntegrityViolationException e) {
+			throw new DatabaseException("Integrity violation");
+		}
+	}
+	
+	
+	private void copyDtoToEntity(ClientDTO dto, Client entity) {
 		entity.setName(dto.getName());
 		entity.setCpf(dto.getCpf());
 		entity.setIncome(dto.getIncome());
 		entity.setBirthDate(dto.getBirthDate());
 		entity.setChildren(dto.getChildren());
 		entity = clientRepository.save(entity);
-		entity = clientRepository.save(entity);		
-		return new ClientDTO(entity);
-		}catch (EntityNotFoundException e) {
-			throw new ResourceNotFoundException("Id not found!");
-		}
-	
 	}
 
 }
